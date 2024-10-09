91c8adb954aedd6fb6898a282add60c78d043209
Samarth Vedi <samarth.vedi@gmail.com>
2024-10-09 02:37:46 -0400

    fix 9

--- src/star_detection.cpp
+++ src/star_detection.cpp
@@ -3,8 +3,16 @@
 std::vector<Star> StarDetection::detectStars(const cv::Mat &image) {
     cv::Mat preprocessed = preprocessImage(image);
     
+    // Ensure the image is single-channel
+    cv::Mat gray;
+    if (preprocessed.channels() > 1) {
+        cv::cvtColor(preprocessed, gray, cv::COLOR_BGR2GRAY);
+    } else {
+        gray = preprocessed;
+    }
+    
     std::vector<cv::Vec3f> circles;
-    cv::HoughCircles(preprocessed, circles, cv::HOUGH_GRADIENT, 1, 30, 100, 30, 3, 10);
+    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, 30, 100, 30, 3, 10);
     
     std::vector<Star> stars;
     for (const auto &circle : circles) {
@@ -19,7 +27,11 @@ std::vector<Star> StarDetection::detectStars(const cv::Mat &image) {
 
 cv::Mat StarDetection::preprocessImage(const cv::Mat &image) {
     cv::Mat gray, blurred;
-    cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    if (image.channels() > 1) {
+        cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    } else {
+        gray = image.clone();
+    }
     cv::GaussianBlur(gray, blurred, cv::Size(5, 5), 0);
     return blurred;
 }
