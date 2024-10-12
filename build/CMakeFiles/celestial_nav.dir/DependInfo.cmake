2a5d616f53310db951b542bdfce47b85216acd67
harishasnain <harishasnain009@gmail.com>
2024-10-11 21:56:59 -0400

    build error

---
+++ build/CMakeFiles/celestial_nav.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/haris/celestial-nav/src/image_acquisition.cpp" "CMakeFiles/celestial_nav.dir/src/image_acquisition.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/image_acquisition.cpp.o.d"
+  "/home/haris/celestial-nav/src/location_determination.cpp" "CMakeFiles/celestial_nav.dir/src/location_determination.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/location_determination.cpp.o.d"
+  "/home/haris/celestial-nav/src/main.cpp" "CMakeFiles/celestial_nav.dir/src/main.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/main.cpp.o.d"
+  "/home/haris/celestial-nav/src/preprocessing.cpp" "CMakeFiles/celestial_nav.dir/src/preprocessing.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/preprocessing.cpp.o.d"
+  "/home/haris/celestial-nav/src/star_detection.cpp" "CMakeFiles/celestial_nav.dir/src/star_detection.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/star_detection.cpp.o.d"
+  "/home/haris/celestial-nav/src/star_matching.cpp" "CMakeFiles/celestial_nav.dir/src/star_matching.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/star_matching.cpp.o.d"
+  "/home/haris/celestial-nav/src/user_interface.cpp" "CMakeFiles/celestial_nav.dir/src/user_interface.cpp.o" "gcc" "CMakeFiles/celestial_nav.dir/src/user_interface.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
